fc766ef3f661d442cb3005332f02237eb1c964db
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-06-28 18:17:24 +0500

    Apply changes recommended by code reviewer

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age, :rental
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name: 'Unknown', parent_permission: true)
@@ -10,7 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @rental = []
+    @rentals = []
   end
 
   def can_use_services?
