d89d4d2c65b2ae498707bcb996645a9f469853c3
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-06-29 19:28:18 +0500

    Define select method allowing user to choose an option

--- main.rb
+++ main.rb
@@ -20,6 +20,26 @@ class Options
     user_choice = gets.chomp
     select_option(user_choice)
   end
+
+  def select_option(user_choice)
+    options = {
+      '1' => :list_all_books,
+      '2' => :list_all_people,
+      '3' => :create_person,
+      '4' => :create_book,
+      '5' => :create_rental,
+      '6' => :list_all_rentals,
+      '7' => :exit
+    }
+
+    method = options[user_choice]
+    if method.nil?
+      puts 'Invalid option selected, please select a number from the list!'
+      show_menu
+    else
+      @app.send(method)
+    end
+  end
 end
 
 def main
