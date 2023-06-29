3934ff922be8fe8b7e42f7f9c3f2e4080fbe0da1
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-06-29 19:36:14 +0500

    Define a method to list all books

--- app.rb
+++ app.rb
@@ -10,4 +10,14 @@ class App
     @people_list = []
     @rentals_list = []
   end
+
+  def list_all_books
+    if @books_list.empty?
+      puts 'There is no any available record! Please, add some books first'
+    else
+      puts 'All available books in the library are as below'
+      @books_list.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+    end
+    @parent.show_menu
+  end
 end
