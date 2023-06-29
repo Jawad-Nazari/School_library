9c07ccdf4a6a5447affc4d0ccb828b2b8f0fd200
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-06-29 19:43:12 +0500

    Define a method to list all people

--- app.rb
+++ app.rb
@@ -20,4 +20,16 @@ class App
     end
     @parent.show_menu
   end
+
+    def list_all_people
+    if @people_list.empty?
+      puts 'There is no any available record! Please, add a person first'
+    else
+      puts 'All avilable persons in the library are as below'
+      @people_list.each do |person|
+        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+    end
+    @parent.show_menu
+  end
 end
