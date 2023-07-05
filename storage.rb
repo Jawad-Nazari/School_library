f66cc74335c337b808fdee60ac5c2e3a339d2125
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-07-05 08:17:58 +0500

    Create class and methodes to store and load data

---
+++ storage.rb
@@ -0,0 +1,29 @@
+require 'json'
+
+class Storage
+  @file_extension = 'json'
+  @base_url = './'
+
+  def self.save_data(class_name, object)
+    file_path = "#{@base_url}#{class_name}.#{@file_extension}"
+    File.open(file_path, 'w') do |file|
+      file.truncate(0) # Truncate the file to remove existing content
+      file.write(JSON.pretty_generate(object))
+    end
+  end
+
+  def self.load_data(class_name)
+    file_path = "#{@base_url}#{class_name}.#{@file_extension}"
+
+    return [] unless File.exist?(file_path)
+
+    begin
+      data_from_file = File.read(file_path)
+      JSON.parse(data_from_file, create_additions: true)
+    rescue JSON::ParserError => e
+      puts "Error parsing JSON file: #{file_path}"
+      puts e.message
+      []
+    end
+  end
+end
