114d644f40ecef0747415399e0c3b45127afa346
Emmanuel <emmanuelnwoye12@gmail.com>
2023-07-05 08:46:46 +0100

    Update rental class to save data

--- rental.rb
+++ rental.rb
@@ -1,9 +1,27 @@
+require_relative 'book'
+require_relative 'person'
+
 class Rental
-  attr_accessor :date, :book, :person
+  attr_reader :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
+  end
+
+  def to_json(*args)
+    {
+      'json_class' => self.class.name,
+      'data' => {
+        'date' => @date,
+        'book' => @book,
+        'person' => @person
+      }
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(object['date'], object['book'], object['person'])
   end
 end
