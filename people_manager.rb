e64eb4c25872b13f4500f9ecf521f32acbfeaaef
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-07-05 08:26:08 +0500

    Update the person.rb and people manager file

--- people_manager.rb
+++ people_manager.rb
@@ -1,17 +1,37 @@
-require_relative 'person'
+require_relative 'teacher'
+require_relative 'student'
+require_relative 'storage'
 
 class PeopleManager
+  attr_reader :people
+
   def initialize
-    @people = []
+    @people = Storage.load_data('people')
   end
 
   def add_person(person)
-    @people << person
+    if person_already_exists?(person)
+      puts 'Person already exists.'
+    else
+      @people << person
+      Storage.save_data('people', people)
+      puts 'Person added successfully.'
+    end
   end
 
   def list_all_people
-    @people.each do |person|
-      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    @people.each_with_index do |person, index|
+      if person.is_a?(Teacher)
+        puts "#{index + 1}) [#{person.class}] Name: #{person.name}, Age: #{person.age}"
+      else
+        puts "#{index + 1}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
     end
+  end
+
+  private
+
+  def person_already_exists?(person)
+    @people.any? { |p| p.id == person.id }
   end
 end
