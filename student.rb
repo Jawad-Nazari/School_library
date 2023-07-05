82c940ef155e0dfe3e41cdc306d9a929fb0efb1a
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-07-05 08:24:24 +0500

    Update the student.rb and teacher.rb files

--- student.rb
+++ student.rb
@@ -17,4 +17,19 @@ class Student < Person
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
+
+  def to_json(*args)
+    {
+      'json_class' => self.class.name,
+      'data' => {
+        'name' => @name,
+        'age' => @age,
+        'classroom' => @classroom
+      }
+    }.to_json(*args)
+  end
+
+  def self.json_create(data)
+    new(data['data']['name'], data['data']['age'], data['data']['classroom'])
+  end
 end
