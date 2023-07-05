d037f961a5db9b7146a8876bb79de17e991909e2
Jawad.Nazari <Jawad.Nazari@outlook.com>
2023-07-05 13:11:20 +0500

    Update person and app files

--- app.rb
+++ app.rb
@@ -5,7 +5,9 @@ require_relative 'rental'
 require_relative 'book'
 require_relative 'book_manager'
 require_relative 'people_manager'
+require_relative 'storage'
 require_relative 'rental_manager'
+
 
 class BookInterface
   def add_book(title, author)
@@ -24,7 +26,9 @@ class BookManagerAdapter < BookInterface
   end
 
   def add_book(title, author)
-    @book_manager.add_book(title, author)
+    book = @book_manager.add_book(title, author)
+    Storage.save_data('books', @book_manager.books)
+    book
   end
 
   def list_all_books
@@ -35,9 +39,10 @@ end
 class App
   def initialize(parent)
     @parent = parent
-    @books_list = []
+    @books_list = Storage.load_data('books')
     @people_list = PeopleManager.new
     @rentals_list = RentalManager.new
+    @rentals_list.load_rentals_from_file('rentals.json') # Load rentals from the JSON file
   end
 
   def list_all_books
@@ -45,7 +50,7 @@ class App
       puts 'There are no available records! Please add some books first.'
     else
       puts 'All available books in the library are as below:'
-      @books_list.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+      @books_list.each { |book| puts "Title: #{book['title']}, Author: #{book['author']}" }
     end
     @parent.show_menu
   end
@@ -88,10 +93,10 @@ class App
     print 'Enter Teacher Name: '
     name = @parent.request_input
     print 'Enter Teacher Age: '
-    age = @parent.request_input
+    age = @parent.request_input.to_i
     print 'Enter Teacher Specialization: '
     specialization = @parent.request_input
-    teacher = Teacher.new(age, specialization, name)
+    teacher = Teacher.new(name, age, specialization)
     @people_list.add_person(teacher)
     puts 'Person created successfully'
   end
@@ -102,33 +107,74 @@ class App
     print 'Enter the author name: '
     author = @parent.request_input
     book = Book.new(title, author)
-    @books_list << book
+    @books_list << { 'title' => book.title, 'author' => book.author }
+    Storage.save_data('books', @books_list)
     puts 'Book created successfully'
     @parent.show_menu
   end
 
   def create_rental
-    puts 'Select a book from the following list by number'
-    @books_list.each_with_index { |book, index| puts "#{index}) Title: '#{book.title}', Author: #{book.author}" }
-    selected_book = @parent.request_input.to_i
+    display_book_list
+    selected_book = select_book
     puts
-    puts 'Select a person from the following list by number (not id)'
-    @people_list.instance_variable_get(:@people).each_with_index do |person, index|
-      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-    selected_person = @parent.request_input.to_i
-    print 'Date: '
-    date = @parent.request_input
-    rental = Rental.new(date, @books_list[selected_book],
-                        @people_list.instance_variable_get(:@people)[selected_person])
+    display_people_list
+    selected_person = select_person
+    date = enter_rental_date
+    rental = Rental.new(date, @books_list[selected_book], @people_list.people[selected_person])
     @rentals_list.add_rental(rental)
+    @rentals_list.save_rentals_to_file('rentals.json')
     puts 'Rental created successfully'
     @parent.show_menu
+  end
+
+
+
+  private
+
+  def display_book_list
+    puts 'Select a book from the following list by number'
+    @books_list.each_with_index do |book, index|
+      puts "#{index}) Title: '#{book['title']}', Author: #{book['author']}"
+    end
+  end
+
+  def select_book
+    @parent.request_input.to_i
+  end
+
+  def display_people_list
+    puts 'Select a person from the following list by number (not id)'
+    @people_list.people.each_with_index do |person, index|
+      display_person_info(person, index + 1)
+    end
+  end
+
+  def display_person_info(person, index)
+    if person.is_a?(Teacher)
+      puts "#{index}) [#{person.class}] Name: #{person.name}, Age: #{person.age}"
+    else
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
+
+  def select_person
+    @parent.request_input.to_i
+  end
+
+  def enter_rental_date
+    print 'Date: '
+    @parent.request_input
+  end
+
+  def create_and_add_rental(selected_book, selected_person, date)
+    rental = Rental.new(date, @books_list[selected_book], @people_list.people[selected_person])
+    @rentals_list.add_rental(rental)
   end
 
   def list_all_rentals
     print 'ID of person: '
     person_id = @parent.request_input.to_i
+    puts "Person ID: #{person_id}"
     puts 'Rentals:'
     @rentals_list.list_all_rentals(person_id)
     @parent.show_menu
--- person.rb
+++ person.rb
@@ -1,18 +1,40 @@
-[
-  {
-    json_class: 'Student',
-    data: {
-      name: 'David',
-      age: 32,
-      classroom: 2
-    }
-  },
-  {
-    json_class: 'Student',
-    data: {
-      name: 'Reza',
-      age: 32,
-      classroom: 1
-    }
-  }
-]
+require_relative 'nameable'
+
+class Person < Nameable
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  def print_of_age
+    puts of_age?
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+end
+
+person1 = Person.new(22, name: 'Jawad')
+person1.print_of_age
